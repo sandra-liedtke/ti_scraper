d51c605b9b4fd3c3b185bc87823025a6835dbebe
S-Liedtke <sandra-liedtke@online.de>
2023-01-06 12:03:39 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# threat_intelligence_scraper
-Scripts for a web scraper getting all newest information on cyber threats
+# ti_scraper
+
+Dieses Repository enthält ein Script, das verwendet werden kann, um die neuesten Artikel zu IT-Sicherheit in einer Datei zu sammeln und/oder per Mail zu verschicken. Das Script kann auch für andere Themen verwendet werden, wenn die URLs in der Konfigurationsdatei getauscht werden.
+
+Einige Konfigurationen sollten eventuell geändert werden, bevor das Script gestartet wird:
+ - Um die Webseiten zu ändern, die durchsucht werden sollen, können in der webpages-Sektion Links hinzugefügt oder gelöscht werden
+ - Es können Stopwords definiert werden, sodass Einträge, die diese Stopwords enthalten, nicht ins Ergebnis geschrieben werden
+ - getDeltaRecords definiert, ob alle Einträge der jeweiligen Webseiten ins Ergebnis geschrieben werden sollen oder nur diejenigen, die noch nicht in vorherigen Dateien auftauchen
+     - HINWEIS: Das Delta-Update funktioniert nur, wenn ein Unterordner für die Ergebnisdateien angegeben wird
+ - In der resultfile-Sektion kann definiert werden, ob eine Datei mit den Ergebnissen erzeugt werden soll, wo diese gespeichert werden und wie sie heißen soll
+     - HINWEIS: Wenn ein Ordnername engegeben wird, der noch nicht existiert, erzeugt das Script den Ordner
+ - Die weiteren Konfigurationen betreffen den Mailversand
+
+Shield: [![CC BY 4.0][cc-by-shield]][cc-by]
+
+Diese Arbeit unterliegt den Bestimmungen einer
+[Creative Commons Namensnennung 4.0 International-Lizenz][cc-by].
+© 2023 Sandra Liedtke
+
+[![CC BY 4.0][cc-by-image]][cc-by]
+
+[cc-by]: https://creativecommons.org/licenses/by/4.0/deed.de
+[cc-by-image]: https://licensebuttons.net/l/by/4.0/88x31.png
+[cc-by-shield]: https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg
+
+
+### English Version
+
+This repository contains a script that can be used to collect the latest news on cyber security in a file and/or send them via mail, but can also be used for other topics.
+
+There are some configurations that may be modified before running the script:
+ - To change the websites to be scraped, just add different links in the webpages section and/or remove existing links
+ - There can be some stopwords defined, to remove some entries which contain these stopwords
+ - getDeltaRecords defines whether all entries currently displayed on the respective webpage should be kept or only those that are not yet listed in existing result files
+     - HINT: The Delta-Update only works if a subfolder is given, where the result files are stored
+ - In the resultfile section can be defined whether a result file should be created, where it should be stored and how it should be named
+     - HINT: If you give a folder name that is not yet existing, the script will create that respective folder
+ - There are also some configurations for sending the result via mail
+
+Shield: [![CC BY 4.0][cc-by-shield]][cc-by]
+
+This work is licensed under a
+[Creative Commons Attribution 4.0 International License][cc-by].
+© 2023 Sandra Liedtke
+
+[![CC BY 4.0][cc-by-image]][cc-by]
+
+[cc-by]: http://creativecommons.org/licenses/by/4.0/
+[cc-by-image]: https://i.creativecommons.org/l/by/4.0/88x31.png
+[cc-by-shield]: https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg
---
+++ main.py
@@ -0,0 +1,161 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+from datetime import datetime
+
+
+# load config
+with open('config/config.json', 'r') as config_file:
+    CONFIG = json.load(config_file)
+
+
+# get current date and add it to the result file name
+TODAY = datetime.now().date()
+# define destination file name depending whether a folder name is given
+if CONFIG['resultfile']['folder']:
+    DEST_FOLDER = CONFIG['resultfile']['folder'] + '/'
+    DEST_FILE_NAME = DEST_FOLDER + str(TODAY) + '_' + CONFIG['resultfile']['filename']
+else:
+    DEST_FILE_NAME = str(TODAY) + '_' + CONFIG['resultfile']['filename']
+
+
+ATAGS = re.compile('<a.*?>')
+CLEAN = re.compile('<article.*?>|<a|class=".*?"|href=|[>]|name=".*?"|title=".*?"|["]')
+
+
+# delta records can only be extracted if a subfolder is given and existing
+LATEST_CONTENT = ''
+if CONFIG['getDeltaRecords'] and DEST_FOLDER:
+        EXISTING_FILES = [os.path.join(DEST_FOLDER, file) for file in os.listdir(DEST_FOLDER)]
+        for file in EXISTING_FILES:
+            # open newest file and read entries
+            with open(file, 'r') as latest_file:
+                LATEST_CONTENT += latest_file.read()
+
+
+def get_urls():
+    urls = CONFIG['websiteconfig']['webpages']
+    return urls
+
+
+def get_webpage(urls):
+    pages = []
+    for webpage in urls:
+        try: 
+            pages.append(requests.get(webpage))
+        except Exception as e:
+            print('Error accessing webpages. Error Message: ', str(e))
+    print('Responses: ', pages)
+    return pages
+
+
+def clean_webpages(websites):
+    articles = []
+    for listentry in websites:
+        try:
+            website_content = BeautifulSoup(listentry.content, 'html.parser')
+            # if "main" tags are not found use "body"
+            if website_content.find('main'):
+                website_content = website_content.find('main')
+            else:
+                website_content = website_content.find('body')
+            # find all a-tags
+            website_content = re.findall(ATAGS, str(website_content))
+            # clean a-tags
+            for entry in website_content:
+                cleaned_result = re.sub(CLEAN, '', entry)
+                # remove unnecessary extracted entries
+                if not any(stopword in cleaned_result for stopword in CONFIG['websiteconfig']['stopwords']):
+                    # add webpage prefix if not already contained in url 
+                    if cleaned_result.strip().startswith("/"):
+                        cleaned_result = str(listentry.url).replace("/fachbeitraege/", '') + cleaned_result.strip()
+                    # add cleaned and filtered entry to result
+                    articles.append(cleaned_result.strip())
+        except Exception as e:
+            print('Error cleaning webpage content for webpage ', str(listentry.url), '. Error Message: ', str(e))
+    return articles
+
+
+def format_result(articles):
+    result_str = ""
+    # remove leftovers
+    try: 
+        [articles.remove(x) for x in articles if not x.startswith("http")]
+        [articles.remove(x) for x in articles if x.startswith("#")]
+        [articles.remove(x) for x in articles if x.startswith("-")]
+        [articles.remove(x) for x in articles if x.startswith("side")]
+        [articles.remove(x) for x in articles if not x]
+        # remove duplicates
+        cleaned_list = []    
+        [cleaned_list.append(x) for x in articles if x not in cleaned_list]
+    except Exception as e:
+        print('Error cleaning result ', str(articles), '. Error Message: ', str(e))
+        print('Continuing processing without cleaning list...')
+    # concatenate list entries to result string
+    for entry in cleaned_list:
+        # special handling for heise links
+        if (not 'heise' in entry) or entry.startswith('https://www.heise.de/security//news/'):
+            # if last character is a / the index of the text is different
+            if entry.endswith('/'):
+                # build and format record for each article to be added to the result
+                new_record = keep_delta(entry.split("/")[len(entry.split("/"))-2].replace('-', ' ').replace('.html', '').upper() + ': ' + str(entry) +   '\n' )
+                result_str += new_record
+            else:
+                # build and format record for each article to be added to the result
+                new_record = keep_delta(entry.split("/")[len(entry.split("/"))-1].replace('-', ' ').replace('.html', '').upper() + ': ' + str(entry).replace('/security//news/', '/security/') + '\n' )
+                result_str += new_record
+    return result_str
+
+
+def write_file(articles):
+    # check if result file folder is there
+    if CONFIG['resultfile']['folder'] and not os.path.exists(DEST_FOLDER):
+        os.makedirs(CONFIG['resultfile']['folder'])
+    try:
+        # create result file
+        with open(DEST_FILE_NAME, 'w') as resultfile:
+            resultfile.write(str(articles))
+    except Exception as e:
+                print('Error writing result file. Error Message: ', str(e))
+
+
+def keep_delta(record):
+    # check if current record is in newest existing file
+    if record in LATEST_CONTENT:
+        # clear record if it already exists in latest file
+        record = ''
+    return record
+
+
+def send_mail(articles):
+    # still a TODO
+    return 'OK'
+
+
+def main():
+    print('\n')
+    print('++++++++++++++++++++++++++++++++++ SCRIPT START ++++++++++++++++++++++++++++++++++')
+    # get urls from config.json
+    print('Getting URLS from config')
+    url_list = get_urls()
+    # call webpages
+    print('Calling webpages as given in config.json')
+    websites = get_webpage(url_list)
+    # clean webpage content and format result
+    print('Cleaning webpage contents')
+    articles = clean_webpages(websites)
+    result = format_result(articles)
+    # write file and/or send as mail depending on config
+    if CONFIG['resultfile']['createFile']:
+        print('Writing result file')
+        write_file(result)
+    if CONFIG['mailconfig']['sendMail']:
+        print('Sending mail')
+        send_mail(result)
+    print('+++++++++++++++++++++++++++++++++++ SCRIPT END +++++++++++++++++++++++++++++++++++')
+
+
+if __name__ == '__main__':
+    main()
