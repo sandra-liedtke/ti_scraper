355ac8a1712f61ef546d97890ca98d5cde8ed395
S-Liedtke <sandra-liedtke@online.de>
2023-02-04 18:49:19 +0100

    fixed comments

--- scripts/lib/regex.py
+++ scripts/lib/regex.py
@@ -3,9 +3,9 @@ import re
 
 # regex for headline between span tags
 HEADLINE_SPAN = re.compile(r'<span class="article-title">(.*?)</span>')
-#headline for headline between div tags
+# regex for headline between header tags
 HEADLINE_HEADER = re.compile(r'<header>(.*?)<header>')
-#headline for headline with h1-h9 tags
+# regex for headline with h1-h9 tags
 HEADLINE_H1 = re.compile(r'<h[0-9].*?>(.*?)</h[0-9]>')
-#headline for headline between div tags
+# regex for headline between div tags
 HEADLINE_DIV = re.compile(r'<div.*?>(.*?)</div>')
