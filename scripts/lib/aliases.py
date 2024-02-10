65ab4df4ef3d1661fda96b53b96937211b68f1f7
Sandra Liedtke <ghub-coding@top-email.net>
2024-02-10 11:29:21 +0100

    Bug fix

--- scripts/lib/aliases.py
+++ scripts/lib/aliases.py
@@ -6,7 +6,6 @@ from lib.commons import CONFIG
 
 
 def update_aliases():
-
     # api instance
     api_instance = demisto_client.configure(base_url=CONFIG['profiling']['CortexXSOARAPIConfig']['host'], debug=False,
                                             verify_ssl=ssl.CERT_NONE,
@@ -15,28 +14,22 @@ def update_aliases():
     # list collecting the aliases
     missing_aliases = []
     # for each profileData entry
-    for indicator_name in CONFIG['profiling']['profileData']:
+    for config_profile in CONFIG['profiling']['profileData']:
         try:
             # search current record in Cortex
             indicator_filter = demisto_client.demisto_api.IndicatorFilter()
-            indicator_filter.query = 'aliases:"' + indicator_name + '"'
+            indicator_filter.query = 'aliases:"' + config_profile + '"'
             found_indicator = api_instance.indicators_search(indicator_filter=indicator_filter)
-            # write those aliases into a list which are not equal to the entry in the config
-            if found_indicator.total > 0:
-                print("Checking aliases for indicator " + indicator_name)
-                for alias in found_indicator.ioc_objects[0]['CustomFields']['aliases']:
-                    if alias not in CONFIG['profiling']['profileData'] and alias not in missing_aliases:
-                        missing_aliases.append(alias)
+            # write those aliases into a list
+            if found_indicator.total == 1:
+                print("Checking aliases for indicator " + config_profile)
+                for cortex_alias in found_indicator.ioc_objects[0]['CustomFields']['aliases']:
+                    if not cortex_alias in str(missing_aliases):
+                        missing_aliases.append(cortex_alias)
         # catch exceptions
         except ApiException as e:
             print(e)
             print("Cannot collect aliases. Aborting...")
-
-    # remove those which are available in config with different case letters
-    for collected_alias in missing_aliases:
-        for indicator in CONFIG['profiling']['profileData']:
-            if collected_alias.upper() == indicator.upper():
-                missing_aliases.remove(collected_alias)
 
     # create the file
     with open('../config/cortex_aliases.json', 'w', encoding='UTF-8') as file:
